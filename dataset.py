f5c6ade99800775beec671f59df38f872a229425
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-12-15 12:25:38 +0900

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -8,7 +8,16 @@ torch.manual_seed(2023)
 
 class CustomDataset(Dataset):
 
+    """
+
+    This class gets several parameters and returns dataset to train an AI model.
+
+    Parameters:
+
+        root             - path to data, str;
+        transformations  - transformations to be applied, torchvision object.    
     
+    """    
     
     def __init__(self, root, data, transformations = None):
         
